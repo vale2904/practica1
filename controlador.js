ea5b69a504b4486cad1e5abd584388949ba42110
vale2904 <70981266+vale2904@users.noreply.github.com>
2021-08-11 16:55:11 -0500

    confi modal

---
+++ controlador.js
@@ -0,0 +1,180 @@
+//CONTROLAR LA ACTIVIDAD DEL FORMULARIO
+let nombreConsola;
+let precioConsola;
+let pesoConsola;
+let fotoConsola;
+let separador=document.getElementById("separador");
+let separador2=document.getElementById("separador2");
+
+//1. Controlar con JS el boton del formulario
+let boton=document.getElementById("boton");
+let botonLimpiar=document.getElementById("botonLimpiar");
+
+//2. Detectar el clic en el boton
+boton.addEventListener("click",capturarDatos);
+botonLimpiar.addEventListener("click",limpiarCarrito);
+
+    pildora.classList.add("invisible");
+    separador.classList.add("invisible");
+    separador2.classList.add("invisible");
+
+//3. Crear funcion PROPIA que se llamo en el punto 2
+function capturarDatos(){
+
+    //4. Capturar los valores del formulario
+    // creo tantas variables como valores del formulario
+
+    let consolaElegida=document.getElementById("productos").value;
+    seleccionarConsola(consolaElegida);
+
+    let cantidad=document.getElementById("cantidad").value;
+    
+    let pildora=document.getElementById("pildora");
+    pildora.textContent=cantidad;
+
+    
+
+    
+    let direccion=document.getElementById("comentarios").value;
+    
+    let fotoCarrito=document.getElementById("fotoCarrito");
+    fotoCarrito.src=fotoConsola;
+
+    let tituloCarrito=document.getElementById("tituloCarrito");
+    tituloCarrito.textContent=nombreConsola;
+
+    let precioIndividual=document.getElementById("precioUnitarioCarrito");
+    precioIndividual.textContent=`Costo unitario: $${precioConsola} USD`;
+
+    let pesoIndividual=document.getElementById("pesoUnitarioCarrito");
+    pesoIndividual.textContent=`Peso unitario: ${pesoConsola} Lb`;
+
+    let pesoTotal=document.getElementById("pesoTotalCarrito");
+    pesoTotal.textContent=`Peso Total del envio: ${pesoConsola*cantidad}Lb`;
+    
+    let costoCasillero=document.getElementById("costoCasillero");
+    costoCasillero.textContent=`Costo Casillero: $${calcularCostoCasillero(pesoConsola,cantidad)} USD`;
+    
+    let costoImpuestos=document.getElementById("costoImpuestos");
+    costoImpuestos.textContent=`Costo venta(impuestos):$${calcularCostoImpuestos(precioConsola,cantidad)}`;
+
+    let costoTotal=document.getElementById("costoTotal");
+    costoTotal.textContent=`Costo Total: $${(calcularCostoCasillero(pesoConsola,cantidad))+(calcularCostoImpuestos(precioConsola,cantidad))} USD`;
+
+    pildora.classList.add("visible");
+    separador.classList.remove("invisible");
+    separador2.classList.remove("invisible");
+
+
+}
+
+//5. Seleccionar la información de la consola segun la selección del usuario
+function seleccionarConsola(opcion){
+
+    let consolas={
+        nombres:Array("PS5","XBOX Serie X", "Nintendo Switch"),
+        precios:Array(750,700,409),
+        pesos:Array(14.2,13.1,4.75),
+        fotos:Array("https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/ps5.jpg?raw=true","https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/xbox.jpg?raw=true","https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/nintendo.jpg?raw=true"),
+        
+    }
+
+    if(opcion==1){
+
+        nombreConsola=consolas.nombres[0];
+        precioConsola=consolas.precios[0];
+        pesoConsola=consolas.pesos[0];
+        fotoConsola=consolas.fotos[0];
+
+    }else if(opcion==2){
+
+        nombreConsola=consolas.nombres[1];
+        precioConsola=consolas.precios[1];
+        pesoConsola=consolas.pesos[1];
+        fotoConsola=consolas.fotos[1];
+
+    }else if(opcion==3){
+
+        nombreConsola=consolas.nombres[2];
+        precioConsola=consolas.precios[2];
+        pesoConsola=consolas.pesos[2];
+        fotoConsola=consolas.fotos[2];
+
+    }else{
+        nombreConsola=null;
+        precioConsola=null;
+        pesoConsola=null;
+        fotoConsola=null;
+
+    }
+
+
+}
+
+function calcularCostoCasillero(pesoConsola,cantidad){
+
+    let costoCasillero=0;
+    let pesoTotalEnvio=pesoConsola*cantidad;
+
+    if(pesoTotalEnvio<=20){
+
+        costoCasillero=85;
+
+    }else{ 
+
+        let pesoExtra=pesoTotalEnvio-20;
+        costoCasillero=85+(pesoExtra*2);
+
+    }
+    
+    return costoCasillero;
+}
+
+function calcularCostoImpuestos(costoConsola,cantidad){
+
+    const IMPUESTO_PAIS=114;
+    const VALOR_SEGURO=7;
+
+    let costoCompra=costoConsola*cantidad;
+    let costoTotal=costoCompra+IMPUESTO_PAIS+VALOR_SEGURO;
+
+    return costoTotal;
+
+} 
+
+
+function limpiarCarrito(){
+
+    console.log("hiciste clic en limpiar");
+    let fotoCarrito=document.getElementById("fotoCarrito");
+    fotoCarrito.src="https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/fotoCarrito.PNG?raw=true";
+  
+    let tituloCarrito=document.getElementById("tituloCarrito");
+    tituloCarrito.textContent="Tu carrito esta vacio";
+
+    
+    let precioIndividual=document.getElementById("precioUnitarioCarrito");
+    precioIndividual.textContent="";
+
+    let pesoIndividual=document.getElementById("pesoUnitarioCarrito");
+    pesoIndividual.textContent="";
+
+    let pesoTotal=document.getElementById("pesoTotalCarrito");
+    pesoTotal.textContent="";
+    
+    let costoCasillero=document.getElementById("costoCasillero");
+    costoCasillero.textContent="";
+    
+    let costoImpuestos=document.getElementById("costoImpuestos");
+    costoImpuestos.textContent="";
+
+    let costoTotal=document.getElementById("costoTotal");
+    costoTotal.textContent="";
+
+    pildora.classList.add("invisible");
+    separador.classList.add("invisible");
+    separador2.classList.add("invisible");
+
+
+
+}
